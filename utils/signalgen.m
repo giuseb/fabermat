b85b4f4718717e8712edd505f04e9a1ad4c9b257
Giuseppe Bertini <giuseppe.bertini@gmail.com>
2016-05-13 10:57:01 +0200

    white noise option added to signalgen

--- utils/signalgen.m
+++ utils/signalgen.m
@@ -1,13 +1,15 @@
-function rv = signalgen(seconds, sampling_rate, params)
+function rv = signalgen(seconds, sampling_rate, params, noise)
    % SIGNALGEN   generating simple signals by summing sinusoidal waves
    %
-   %     SIGNALGEN(SECONDS, SAMPLING_RATE, PARAMS)
+   %     SIGNALGEN(SECONDS, SAMPLING_RATE, PARAMS, NOISE)
    %     generates a SECONDS-long signal at SAMPLING_RATE Hertz, with the
    %     given PARAMS. The latter is a two-column numerical matrix, where
    %     each row defines a sinusoidal wave. The first column is the
    %     periodicity in Hertz, the second is the amplitude.
    %     With a single PARAMS row, the signal is a perfect sinusoid; the
    %     more rows are added, the noisier the signal.
+   %     White noise of NOISE amplitude is added, if the argument is
+   %     provided.
    %
    % Last modified: 6 May 2016
    
@@ -23,4 +25,7 @@ function rv = signalgen(seconds, sampling_rate, params)
    amplitude = repmat(params(:,2), 1, size(t,2));
    % computing the signal
    rv = sum(amplitude .* sin(2*pi*wave_hz.*t), 1);
+   if nargin==4
+      rv = rv + randn(size(rv)) * noise;
+   end
 end
